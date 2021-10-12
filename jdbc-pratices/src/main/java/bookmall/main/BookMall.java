ebeb5abee03477267672015166e4511fec20b663
Dongjin Lee <donghs1541>
2021-10-12 09:40:13 +0900

    bookmalldao

--- jdbc-pratices/src/main/java/bookmall/main/BookMall.java
+++ jdbc-pratices/src/main/java/bookmall/main/BookMall.java
@@ -11,25 +11,32 @@ public class BookMall {
 
 	public static void main(String[] args) {
 		
+		System.out.println("--- 회원리스트 ---");
 		MemberDaoTest.insertTest();
 		MemberDaoTest.findAllTest();
 		System.out.println();
+		
+		System.out.println("--- 카테고리 ---");
 		CategoryDaoTest.insertTest();
 		CategoryDaoTest.findAllTest();
 		System.out.println();
 		
+		System.out.println("--- 상품리스트 ---");
 		BookDaoTest.insertTest();
 		BookDaoTest.findAllTest();
 		System.out.println();
-
+		
+		System.out.println("--- 카트리스트 ---");
 		CartDaoTest.insertTest();
 		CartDaoTest.findAllTest();
 		System.out.println();
-
+		
+		System.out.println("--- 주문리스트 ---");
 		OrderDaoTest.insertTest();
 		OrderDaoTest.findAllTest();
 		System.out.println();
-
+		
+		System.out.println("--- 주문도서리스트 ---");
 		OrderBookDaoTest.insertTest();
 		OrderBookDaoTest.findAllTest();
 		
